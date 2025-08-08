35a1ee4c665d0671107a23e09cdc1a582fff4e46
Hariveeraprasad-007 <arikatlahariveeraprasad007@gmail.com>
2025-08-08 18:36:30 +0530

    Code for toss is written

--- Main.java
+++ Main.java
@@ -14,4 +14,27 @@ public class Main{
         }while(playagain);
         System.out.println("Thanks for playing "+name);
     }
+    public static void playmatch(){
+        String toss_choice;
+        String diffculty;
+        boolean batting_first;
+        System.out.println("**** TOSS TIME ****");
+        System.out.println("Choose odd or even");
+        String choice=scanner.next().to_LowerCase();
+        System.out.println("Enter a number in between 1-10");
+        int user_num=getvaildnumber();
+        int computer_num=random.nextInt(10)+1;
+        int total=user_num+computer_num;
+        boolean toss_result=(choice.equals("odd")&& total%2!=0)|(choice.equals("even")&&(total%2==0))
+        if(toss_result){
+            System.out.println(name+" You have won toss,what could like to do? would bat first or bowl first(bat or bowl)");
+            String user_choice=scanner.next().to_LowerCase();
+            boolean player_batting_first=user_choice.equals("bat");
+        }
+        else{
+            System.out.println("Your Oppent won the toss");
+            player_batting_first=random.nextboolean();
+            System.out.println("Your Oppent choosed "+(player_batting_first?"bowl":"bat")+" first");
+        }
+    }
 }
